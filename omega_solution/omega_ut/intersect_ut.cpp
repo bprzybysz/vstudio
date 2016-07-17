1eda9e4c6b12501fbf19a60eb3c2c128f042e745
Błażej Przybyszewski <blazej.przybyszewski@gmail.com>
2016-07-17 14:45:26 +0200

    removing

--- omega_solution/omega_ut/intersect_ut.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "../omega/intersect.h"
-#include "gtest/gtest.h"
-
-
-TEST(intersectTests, test1)
-{
-	EXPECT_EQ(intersect::solution(0, 2, 5, 10, 3, 1, 20, 15), 16);
-}
